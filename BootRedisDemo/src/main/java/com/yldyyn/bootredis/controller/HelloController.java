0a49176c65d18329dcf1b83fcb23faf7dd14daf5
yinlidong77 <yinlidong1995>
2019-02-26 14:43:24 +0800

    thiree commit All

--- BootRedisDemo/src/main/java/com/yldyyn/bootredis/controller/HelloController.java
+++ BootRedisDemo/src/main/java/com/yldyyn/bootredis/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.yldyyn.bootredis.controller;
 
+import com.yldyyn.bootredis.config.RedisConifg;
 import com.yldyyn.bootredis.service.TestService;
+import com.yldyyn.bootredis.utils.RedisUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +20,6 @@ import java.util.Map;
 public class HelloController {
     @Resource
     private TestService testService;
-
 
     @RequestMapping("hello")
     @ResponseBody
