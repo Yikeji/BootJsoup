d6e2aee63d9d8093c26e0c5cde191895206fd158
yinlidong77 <yinlidong1995>
2019-03-07 16:13:13 +0800

    thymeleaf,和modelandView解析代码

--- BootRedisDemo/src/main/java/com/yldyyn/bootredis/controller/HelloController.java
+++ BootRedisDemo/src/main/java/com/yldyyn/bootredis/controller/HelloController.java
@@ -4,10 +4,16 @@ import com.yldyyn.bootredis.config.RedisConifg;
 import com.yldyyn.bootredis.service.TestService;
 import com.yldyyn.bootredis.utils.RedisUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -40,6 +46,27 @@ public class HelloController {
         String key = "name";
         System.out.println("缓存中获取到的值："+redisUtils.get(key));
         return allCourse;
-
     }
+    @GetMapping("/returnModel")
+    public ModelAndView returnModel(){
+        ModelAndView modelAndView = new ModelAndView("/index");
+        String testname = "三";
+        String testage = "18";
+        modelAndView.addObject("testname",testname);
+        modelAndView.addObject("testage",testage);
+        return modelAndView;
+    }
+
+    @GetMapping("templates")
+    public String testmodel(HttpServletRequest request,Model model){
+        request.setAttribute("key","测试数据");
+        return "index";
+    }
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        String testname = "三";
+        model.addAttribute("name", testname);
+        return "index";
+    }
+
 }
